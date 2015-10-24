22b680d538db8336cf8474542e90a1cb684b6984
abe_clark <abeaclark@gmail.com>
2015-10-24 11:19:00 -0700

    change to single quotes

--- javascript/api.js
+++ javascript/api.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 
 $('button').click(function() {
 
-    var url = "http://api.target.com/items/v3/055-02-3741";
+    var url = 'http://api.target.com/items/v3/055-02-3741';
     var data = {
       // product_id : "055-02-3741",
-      id_type : "dpci",
-      store_id : "530",
-      fields : ["ids","descriptions","locations","pricing","images"],
-      mode : "online",
-      key : "1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb"
+      id_type : 'dpci',
+      store_id : '530',
+      fields : ['ids','descriptions','locations','pricing','images'],
+      mode : 'online',
+      key : '1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb'
     };
 
     $.getJSON(url, data, function (response) {
