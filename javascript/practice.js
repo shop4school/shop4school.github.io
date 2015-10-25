24c9968f30bbf397ef77528e8e4ef0fffb57a880
abe_clark <abeaclark@gmail.com>
2015-10-24 22:55:14 -0700

    try treehouse method

--- javascript/practice.js
+++ javascript/practice.js
@@ -3,14 +3,18 @@ $(document).ready(function () {
   $('button').click(function() {
 
     $.ajax ({
-      type: 'GET',
-      dataType: "jsonp",
-      jsonp: 'jsonp',
-      crossDomain: 'true',
       url: 'http://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb',
+      jsonpCallback: 'jsonCallback',
+      consentType: "application/json",
+      dataType: 'jsonp',
+
       succes: function(data) {
         console.log('success', data);
       } //end success function
+
+      error: function(e) {
+        console.log(e.message);
+      } //end error function
 
     }); // end AJAX
 
