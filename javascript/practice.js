27c1362ba02ee2e4d2df0a85c175453559259311
abe_clark <abeaclark@gmail.com>
2015-10-24 23:13:17 -0700

    mod

--- javascript/practice.js
+++ javascript/practice.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
 
       succes: function(json) {
         console.log(json);
-        $("#output").html(json.product_composite_response.items.online_description.value);
-        $("#output").html(json.product_composite_response.items.online_price.current_price);
-        $("#output").html(product_composite_response.items.online_price.original_price);
-        $("#output").html(json.product_composite_response.items.image.internal_primary_image_url);
+        $("#output-1").html(json.product_composite_response.items.online_description.value);
+        $("#output-2").html(json.product_composite_response.items.online_price.current_price);
+        $("#output-3").html(product_composite_response.items.online_price.original_price);
+        $("#output-4").html(json.product_composite_response.items.image.internal_primary_image_url);
       }, //end success function
 
       error: function(e) {
