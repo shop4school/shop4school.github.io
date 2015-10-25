93c05d8407f3160b1b5ccd21b3fd037dcf490dc2
abe_clark <abeaclark@gmail.com>
2015-10-24 21:13:22 -0700

    Add practice JS file

---
+++ javascript/practice.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  $('button').click(function() {
+
+    $.ajax ({
+      type: 'GET',
+      url: 'https://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb'
+      succes: function(data) {
+        console.log('success', data)
+
+      }; //end success function
+
+    }); // end AJAX
+
+  }); // end button.click
+
+}); // end ready
