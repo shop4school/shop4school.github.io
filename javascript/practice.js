3edc1731dd3996257731028d131353564f8d0831
abe_clark <abeaclark@gmail.com>
2015-10-24 22:02:16 -0700

    another dumb change

--- javascript/practice.js
+++ javascript/practice.js
@@ -5,9 +5,8 @@ $(document).ready(function () {
     $.ajax ({
       type: 'GET',
       dataType: "jsonp",
-      jsonp: 'jsonp'
-      Access-Control-Allow-Origin: *,
-      url: 'https://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb',
+      jsonp: 'jsonp',
+      url: 'http://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb',
       succes: function(data) {
         console.log('success', data);
       } //end success function
