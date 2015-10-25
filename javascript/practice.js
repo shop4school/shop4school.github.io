d212bd2b2b81ffe2ad701f42f7064fa36cc8ba71
shop4school <abrahamclark@hotmail.com>
2015-10-24 21:16:56 -0700

    Update practice.js

--- javascript/practice.js
+++ javascript/practice.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $.ajax ({
       type: 'GET',
-      url: 'https://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb'
+      url: 'https://api.target.com/items/v3/055-02-3741?id_type=dpci&store_id=530&fields=ids,descriptions,locations,pricing,images&mode=online&key=1Kfdqvy6wHmvJ4LDyAVOl7saCBoKHcSb',
       succes: function(data) {
         console.log('success', data)
 
