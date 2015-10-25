fdb08fa5453d6c89c114c89964eb2ec10c8def89
abe_clark <abeaclark@gmail.com>
2015-10-24 23:45:23 -0700

    0

--- javascript/practice.js
+++ javascript/practice.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
 
       success: function(json) {
         console.log(json);
-        $("#output-1").html(json.product_composite_response.items.0.online_description);
-        $("#output-2").html(json.product_composite_response.items.0.online_price.current_price);
-        $("#output-3").html(json.product_composite_response.items.0.online_price.original_price);
-        $("#output-4").html(json.product_composite_response.items.0.image.internal_primary_image_url);
+        $("#output-1").html(json.product_composite_response.items.O.online_description);
+        $("#output-2").html(json.product_composite_response.items.O.online_price.current_price);
+        $("#output-3").html(json.product_composite_response.items.O.online_price.original_price);
+        $("#output-4").html(json.product_composite_response.items.O.image.internal_primary_image_url);
         }, //end success function
 
        error: function(e) {
